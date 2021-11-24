5592d08e80e6fff71932062df80df457f71bc7d0
Martin Dymenstein <martin.dymenstein@matera.com>
2021-11-23 23:51:31 -0300

    terraform iac

--- terraform-ec2.tf
+++ terraform-ec2.tf
@@ -2,10 +2,12 @@ provider "aws" {
     region = "us-east-1"
 }
 
-#variable "ec2_names" {
-#    type = list(string)
-#    default = ["nginx","server1","server2","database","redis"]
-#}
+data "aws_availability_zones" "all" {}
+
+variable "ec2_names" {
+    type = list(string)
+    default = ["server1","server2","database","redis"]
+}
 
 locals {
 
@@ -39,20 +41,6 @@ locals {
        }
      ]
      ec2_instances = [
-       {
-            name = "nginx",
-            ip_address = "172.31.16.4",
-            depends_on = [],
-            script = <<-EOF
-                        #!/bin/bash
-                        sudo apt-get update 
-                        sudo apt install docker -y       
-                        curl -sSL https://get.docker.com/ | sh 
-                        sudo service docker start 
-                        sudo usermod -a -G docker ubuntu
-                     EOF
-            
-       },
        {
             name = "database",
             ip_address = "172.31.16.7",
@@ -135,31 +123,25 @@ resource "aws_security_group" "terraform_segurity_group" {
      }
 }
 
-resource "aws_instance" "terraform_ec2_servers" {
-    #for_each = toset(var.ec2_names)
-    for_each = {
-       for index, vm in local.ec2_instances_servers:
-       index => vm
-    }
-    
-    ami = "ami-083654bd07b5da81d"
-    instance_type = "t2.micro"
-    private_ip = each.value.ip_address
-    vpc_security_group_ids = [
-         aws_security_group.terraform_segurity_group.id
-    ]
- #   provisioner "file" {
- #        source = "install-docker.sh"
- #        destination = "/tmp/install-docker.sh"
- #   }
-    key_name   = "mdymen"    
-    subnet_id = "subnet-056ceb1856e2d326e"
-    tags = {
-        Name = each.value.name
-    }
-    depends_on = [aws_instance.terraform_ec2_example]
-    user_data = each.value.script  
-}
+#resource "aws_instance" "terraform_ec2_servers" {
+#    for_each = {
+#       for index, vm in local.ec2_instances_servers:
+#       index => vm
+#    }
+#    ami = "ami-083654bd07b5da81d"
+#    instance_type = "t2.micro"
+#    private_ip = each.value.ip_address
+#    vpc_security_group_ids = [
+#         aws_security_group.terraform_segurity_group.id
+#    ]
+#    key_name   = "mdymen"    
+#    subnet_id = "subnet-056ceb1856e2d326e"
+#    tags = {
+#        Name = each.value.name
+#    }
+#    depends_on = [aws_instance.terraform_ec2_example]
+#    user_data = each.value.script  
+#}
 
 resource "aws_instance" "terraform_ec2_example" {
     #for_each = toset(var.ec2_names)
@@ -192,7 +174,7 @@ resource "aws_elb" "elb" {
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
 
   listener {
-    instance_port     = 8001
+    instance_port     = 8081
     instance_protocol = "http"
     lb_port           = 8081
     lb_protocol       = "http"
@@ -206,15 +188,45 @@ resource "aws_elb" "elb" {
     interval            = 30
   }
 
-  instances                   = [aws_instance.terraform_ec2_servers.id]
-#  cross_zone_load_balancing   = true
-#  idle_timeout                = 400
-#  connection_draining         = true
-#  connection_draining_timeout = 400
-
   tags = {
     Name = "terraform-elb"
   }
 }
 
+resource "aws_launch_configuration" "terraform_launch" {
+    name = "servidor"
+    image_id = "ami-083654bd07b5da81d"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.terraform_segurity_group.id]
+    lifecycle {
+       create_before_destroy = true
+    }   
+    key_name   = "mdymen"  
+    depends_on = [aws_instance.terraform_ec2_example]
+    user_data = <<-EOF
+                        #!/bin/bash                     
+                        sudo apt-get update 
+                        sudo apt install docker -y       
+                        curl -sSL https://get.docker.com/ | sh 
+                        sudo service docker start 
+                        sudo usermod -a -G docker ubuntu                        
+                        sudo docker run -p 8081:8080 --name concurrency -e MYSQL_HOST=172.31.16.7 -e REDIS_HOST=172.31.16.8 -d mdymen85/concurrency:latest                        
+                     EOF 
+}
+
+resource "aws_autoscaling_group" "terraform_asg" {
+   launch_configuration = aws_launch_configuration.terraform_launch.id
+   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
+   
+   min_size = 2
+   max_size = 10
+   
+   load_balancers = [aws_elb.elb.name]   
+   
+   tag {
+      key = "name"
+      value = "terraform_asg_first"
+      propagate_at_launch = true
+   }
+}
 
